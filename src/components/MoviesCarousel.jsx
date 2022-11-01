98d48ad35d24087b77c625e8b76b589b674702f6
kyawlinnthant <kyawlinnthant.mhs@gmail.com>
2022-11-01 14:21:03 +0630

    detail api

--- src/components/MoviesCarousel.jsx
+++ src/components/MoviesCarousel.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { base_url } from "../utils/tmdbApi";
+import { useNavigate } from "react-router-dom";
 
 const MoviesCarousel = ({ data, error, loading }) => {
+  const navigate = useNavigate();
+
   if (loading) return <p>loading..</p>;
   if (error) return console.log(error.message);
-  console.log(data);
+
+  const handleClick = (id) => {
+    navigate(`/movies/${id}`);
+  };
   return (
     <div className="">
       {data?.map((movie) => (
         <div className="w-[500px] h-[500px]">
-          <h1 className="font-2xl font-bold">{movie?.title}</h1>
+          <h1
+            className="font-3xl font-bold cursor-pointer hover:bg-green-50"
+            onClick={() => handleClick(movie.id)}
+          >
+            {movie?.title}
+          </h1>
           <img
             src={`${base_url}${movie?.backdrop_path}`}
             alt="cover"
--- src/pages/MovieInformationScreen.jsx
+++ src/pages/MovieInformationScreen.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import { getMovieDetail, base_url } from "../utils/tmdbApi";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 const MovieInformationScreen = () => {
-  return <div>MovieInformationScreen</div>;
+  const { id } = useParams();
+
+  const {
+    isLoading,
+    isError,
+    error,
+    data: detail,
+  } = useQuery(["movieDetail", id], () => getMovieDetail(id));
+
+  if (isLoading)
+    return <p className="h-full w-full absolute inset-0">Loading...</p>;
+
+  return (
+    <div className="flex bg-slate-50 justify-center items-center flex-col max-w-[300px] pt-20 h-auto">
+      <div key={detail?.data?.id} className="w-[200px] h-[100px] flex flex-col">
+        <img
+          src={`${base_url}${detail?.data?.backdrop_path}`}
+          alt="cover"
+          className="w-full h-full object-contain"
+        />
+        <h1 className="text-2xl font-medium text-black pb-5">
+          {detail?.data?.title}
+        </h1>
+        <p className="font-base text-gray-500">{detail?.data?.overview}</p>
+        <Link to="/">Close</Link>
+      </div>
+    </div>
+  );
 };
 
 export default MovieInformationScreen;
